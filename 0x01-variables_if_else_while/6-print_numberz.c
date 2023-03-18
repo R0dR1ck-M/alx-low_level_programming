50aba383b66f6aada7bb787ac15517f1ebaac1d6
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-18 03:14:55 -0700

    Betty satisfy

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,12 @@
 */
 int main(void)
 {
-int n;
-for (n = '0'; n<='9'; n++)
-{
-	putchar(n);
-{
-putchar ('\n');
-return (0);
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar ('\n');
+	return (0);
 }
