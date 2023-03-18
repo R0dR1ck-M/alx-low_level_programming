dade25b6dd212ecc57d2acc782f8fcea1c531c37
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-18 03:34:17 -0700

    Correction

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,11 +8,11 @@ include <stdio.h>
 */
 int main(void)
 {
-char c;
-for (c = 'z'; c >= 'a'; c--)
-}
-putchar(c);
-{
-putchar('\n\);
-return (0);
+	char ch;
+	for (ch = 'z'; ch >= 'a'; c--)
+	{
+		putchar(ch);
+	}
+	putchar('\n\);
+	return (0);
 }
