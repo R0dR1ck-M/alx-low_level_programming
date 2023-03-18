b195bf0c1a7036c2ee35c6f114f0483085e15b70
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-17 19:37:00 -0700

    Betty yawah

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,14 +5,14 @@
  * This program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int c;
 
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
-		if(c != 57)
+	if(c != 57)
 		{
 			putchar(',');
 			putchar(' ');
