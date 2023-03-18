c72562807b073a55c1a48a7d1f6e3f569210d99c
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-18 02:28:07 -0700

    Betty satisfy

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 *
 * Description: using the main function
 * this program prints "Programming is positive, zero or negative"
-* Return:0
+* Return: Always 0
 */
 int main(void)
 {
@@ -14,8 +14,8 @@ int n;
 int l;
 
 srand(time(0|);
-n = rand () - RAND_MAX / 2;
-l = n % 10;
+n = rand() - RAND_MAX / 2;
+l = (n % 10);
 
 if (l > 5)
 {
