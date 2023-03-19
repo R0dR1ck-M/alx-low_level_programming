bceab443da586d68e77f4c55d11288a9a8966ec6
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-19 03:19:13 -0700

    Correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,22 +6,22 @@
 * no repetition of different combinations of same digits
 * Return - Always 0 (Success)
 */
-int main (void)
+int main(void);
 {
 	int n, m, l;
 
-	for(n = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for(m = 49; m < 58; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for(l = 50; l < 58; l++)
+			for (l = 50; l < 58; l++)
 			{
-				if(l > m && m > n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if(n != 55 || m != 56)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
