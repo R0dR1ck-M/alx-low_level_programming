6416c7c396f7909e8257f30293785580ac69a90c
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-19 04:39:49 -0700

    Correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
- *
- * Return to 0 if sucess
- */
+* main - prints out a string to stdout.
+* Description - This program prints "with proper grammar, but the outcome is a piece of art,"
+* followed by a new line
+* Return to 0 if success
+*/
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
