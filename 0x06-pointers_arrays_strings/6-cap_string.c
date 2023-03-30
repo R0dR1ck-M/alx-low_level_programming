378f54fda3487d4c045a1c10fdd8af38c8713c7e
R0dR1ck-M <rodrickmutai@gmail.com>
2023-03-30 07:53:31 -0700

    Fixes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,30 +4,30 @@
  * cap_string - capitalizes all words of a string
  * @s: string to capitalize
  *
- * Return: address of s
+ * Return: The modified string
  */
 
 char *cap_string(char *s)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int i, j;
 
-	while(*(s + i))
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
+			if(s[i] == spe[j])
 			{
-				for (j = 0; j <= 12; j++)
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
+					s[i + 1] -= 32;
 				}
 			}
 		}
-		i++;
 	}
 	return (s);
 }
