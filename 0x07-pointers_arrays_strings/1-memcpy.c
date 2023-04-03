947a8eb722e61ec34d6b43e2058c5a7025c50ad6
R0dR1ck-M <rodrickmutai@gmail.com>
2023-04-03 11:15:09 -0700

    Fixes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _memcpy - copies a memory area
-* @dest: memory area to be copied to
-* @src: memory area to be copied from
+*_memcpy - copies a memory area
+*@dest: memory area to be copied to
+*@src: memory area to be copied from
 *@n: number of bytes to be copied
-* Return: pointer to the copied memory block
+*Return: pointer to the copied memory block
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
-* print_diagsums - prints the sums of the two diagonals of a square matrix
-* @a: pointer to start of matrix
-* @size: width of matrix column
-* Return; void
+*print_diagsums - prints the sums of the two diagonals of a square matrix
+*@a: pointer to start of matrix
+*@size: width of matrix column
+*Return: void
 */
 
 void print_diagsums(int *a, int size)
